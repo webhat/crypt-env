900e50ffb9efb2415b4e8125aac496cd66ad1926
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-11-30 00:37:45 +0100

    Initial commit

---
+++ lib/crypt-env.rb
@@ -0,0 +1,2 @@
+module CryptEnv
+end
---
+++ lib/crypt-env/version.rb
@@ -0,0 +1,10 @@
+# in lib/crypt-env/version.rb
+module CryptEnv
+  module Version
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 2
+
+    STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+  end
+end
